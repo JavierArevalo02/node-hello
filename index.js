bfcd05e570102eff57f349c50f4a3dfcd261b45c
CARVAJAL\javareca <javier.arevalo@carvajal.com>
2022-10-04 19:47:53 -0500

    prueba de jenkins

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'This is a new test for Jenkins class homework.'
+  const msg = 'This is a new test for Jenkins class homework. 1'
   res.end(msg);
 });
 
